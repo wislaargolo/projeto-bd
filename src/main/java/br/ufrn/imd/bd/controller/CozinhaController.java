4931c0782c342189f45cdc969f19e576acbfd929
wislaargolo <wislaargolo1@gmail.com>
2024-06-05 16:10:31 -0300

    fix: corrigir relacao entre CozinhaController e ProdutoController

--- src/main/java/br/ufrn/imd/bd/controller/CozinhaController.java
+++ src/main/java/br/ufrn/imd/bd/controller/CozinhaController.java
@@ -1,74 +1,28 @@
 package br.ufrn.imd.bd.controller;
 
-import br.ufrn.imd.bd.exceptions.EntidadeJaExisteException;
 import br.ufrn.imd.bd.model.Cozinheiro;
-import br.ufrn.imd.bd.model.InstanciaProduto;
 import br.ufrn.imd.bd.service.ProdutoService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.SQLException;
 import java.util.List;
 
 @Controller
 @RequestMapping("/cozinha")
-public class CozinhaController {
+public class CozinhaController extends ProdutoController {
 
     @GetMapping
-    public String home() {
+    public String home() throws SQLException {
         return "redirect:/pedidos";
     }
 
-    @Autowired
-    private ProdutoService produtoService;
 
-    @GetMapping("/produtos")
-    public String listarProdutos (Model model) throws SQLException {
-        model.addAttribute("layout", "cozinha/layout");
-        model.addAttribute("instanciaProdutoList", produtoService.buscarTodos());
-        return "produto/lista";
-    }
-
-    @GetMapping("/produtos/novo")
-    public String criarFormProduto(Model model) {
-        model.addAttribute("layout", "cozinha/layout");
-        model.addAttribute("instanciaProduto", new InstanciaProduto());
-        return "produto/formulario";
-    }
-
-    @PostMapping("/produtos/salvar")
-    public String salvarProduto(@ModelAttribute @Valid InstanciaProduto instanciaProduto,
-                                BindingResult bindingResult, Model model) throws SQLException, EntidadeJaExisteException {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("layout", "cozinha/layout");
-            return "produto/formulario";
-        }
-        produtoService.salvar(instanciaProduto);
-        return "redirect:/cozinha/produtos";
-    }
-
-    @GetMapping("produtos/{id}/editar")
-    public String editarProduto(Model model, @PathVariable Long id) throws SQLException {
-        model.addAttribute("layout", "cozinha/layout");
-        model.addAttribute("instanciaProduto", produtoService.buscarPorId(id));
-        return "produto/formulario";
-    }
-
-    @GetMapping("produtos/{id}/excluir")
-    public String excluirProduto(@PathVariable Long id) throws SQLException {
-        produtoService.deletar(id);
-        return "redirect:/cozinha/produtos";
+    @Override
+    public String getLayout() {
+        return "cozinha";
     }
 }
-
-
-
--- src/main/java/br/ufrn/imd/bd/controller/ProdutoController.java
+++ src/main/java/br/ufrn/imd/bd/controller/ProdutoController.java
@@ -14,44 +14,49 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
 import java.util.List;
-//esse controller vai sumir
-@Controller
-@RequestMapping("/produtos")
-public class ProdutoController {
+public abstract class ProdutoController {
 
     @Autowired
     private ProdutoService produtoService;
 
-    @GetMapping
+    public abstract String getLayout();
+
+    @GetMapping("/produtos")
     public String listarProdutos (Model model) throws SQLException {
+        model.addAttribute("layout", getLayout() + "/layout");
         model.addAttribute("instanciaProdutoList", produtoService.buscarTodos());
         return "produto/lista";
     }
 
-    @GetMapping("/novo")
+    @GetMapping("/produtos/novo")
     public String criarFormProduto(Model model) {
+        model.addAttribute("layout", getLayout() + "/layout");
         model.addAttribute("instanciaProduto", new InstanciaProduto());
         return "produto/formulario";
     }
 
-    @PostMapping
-    public String salvarProduto(@ModelAttribute @Valid InstanciaProduto instanciaProduto, BindingResult bindingResult) throws SQLException, EntidadeJaExisteException {
+    @PostMapping("/produtos/salvar")
+    public String salvarProduto(@ModelAttribute @Valid InstanciaProduto instanciaProduto,
+                                BindingResult bindingResult, Model model) throws SQLException, EntidadeJaExisteException {
         if (bindingResult.hasErrors()) {
+            model.addAttribute("layout", getLayout() + "/layout");
             return "produto/formulario";
         }
         produtoService.salvar(instanciaProduto);
-        return "redirect:/produtos";
+        return "redirect:/" + getLayout() + "/produtos";
     }
 
-    @GetMapping("/{id}/editar")
+    @GetMapping("produtos/{id}/editar")
     public String editarProduto(Model model, @PathVariable Long id) throws SQLException {
+        model.addAttribute("layout", getLayout() + "/layout");
         model.addAttribute("instanciaProduto", produtoService.buscarPorId(id));
         return "produto/formulario";
     }
 
-    @GetMapping("/{id}/excluir")
+    @GetMapping("produtos/{id}/excluir")
     public String excluirProduto(@PathVariable Long id) throws SQLException {
         produtoService.deletar(id);
-        return "redirect:/produtos";
+        return "redirect:/" + getLayout() + "/produtos";
     }
 }
+
